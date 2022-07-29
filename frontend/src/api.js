9142fbecb194ef8e4c6626d0dd4aca6bd94a64df
nichro02 <nichro02@gmail.com>
2022-07-28 18:34:30 -0600

    Started building API to fetch cities based on search

---
+++ frontend/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3ab63b1610mshb36565b534e2d90p1ff314jsn43ab8cb0a51f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
--- frontend/src/components/search/search.js
+++ frontend/src/components/search/search.js
@@ -1,9 +1,17 @@
 import { useState } from 'react'
-import { AsyncPaginate } from "react-select-async-paginate"
+import { AsyncPaginate } from 'react-select-async-paginate'
+import { GEO_API_URL, geoApiOptions} from '../../api'
 
 const Search = ({onSearchChange}) => {
     
     const [search, setSearch] = useState('')
+
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=50000&namePrefix=${inputValue}`, geoApiOptions)
+	    .then(response => response.json())
+	    .then(response => console.log(response))
+	    .catch(err => console.error(err));
+    }
 
     const handleOnChange = (searchData) => {
         setSearch(searchData)
@@ -16,6 +24,7 @@ const Search = ({onSearchChange}) => {
             debounceTimeout = {600}
             value = {search}
             onChange = {handleOnChange}
+            loadOptions ={loadOptions}
         />
     )
 }
