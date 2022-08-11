d70dc28c1cab886e63f40b52cc9f0568252257d1
nichro02 <nichro02@gmail.com>
2022-08-10 19:03:10 -0600

    Finished building currentWeather component structure

--- frontend/src/components/currentWeather/currentWeather.js
+++ frontend/src/components/currentWeather/currentWeather.js
@@ -10,6 +10,31 @@ const CurrentWeather = () => {
         </div>
         <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
+
+      <div className='bottom'>
+        <p className='temperature'>x°C</p>
+        <div className='details'>
+            <div className='parameter-row'>
+                <span className='parameter-label top'>Details</span>
+            </div>
+            <div className='parameter-row'>
+                <span className='parameter-label'>Feels like</span>
+                <span className='parameter-value'>Temperature</span>
+            </div>
+            <div className='parameter-row'>
+                <span className='parameter-label'>Wind</span>
+                <span className='parameter-value'>Wind speed</span>
+            </div>
+            <div className='parameter-row'>
+                <span className='parameter-label'>Humidity</span>
+                <span className='parameter-value'>Humidity %</span>
+            </div>
+            <div className='parameter-row'>
+                <span className='parameter-label'>Pressure</span>
+                <span className='parameter-value'>Amount of pressure</span>
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
