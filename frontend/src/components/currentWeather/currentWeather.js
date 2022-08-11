f1091ebc22df1f37fe24a9a9c1c45e88a9789869
nichro02 <nichro02@gmail.com>
2022-08-10 18:38:43 -0600

    Imported weather icons and set up currentWeather component

--- frontend/src/components/currentWeather/currentWeather.js
+++ frontend/src/components/currentWeather/currentWeather.js
@@ -1,7 +1,15 @@
 import './currentWeather.css'
 
 const CurrentWeather = () => {
-    return 'hello world'
+    return (
+        <div className='weather'>
+            <div className='top'>
+                <p className='city'>City:</p>
+                <p className='weather-description'>Weather</p>
+            </div>
+            <img alt='weather' className='weather-icon' src='icons/01d.png' />
+        </div>
+    )
 }
 
 export default CurrentWeather
