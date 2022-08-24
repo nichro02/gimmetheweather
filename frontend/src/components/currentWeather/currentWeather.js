2b39fd0e1049016d5c59f2bd80797b72b9b30b6d
nichro02 <nichro02@gmail.com>
2022-08-23 18:42:15 -0600

    Cleaned up CurrentWeather formatting and units

--- frontend/src/components/currentWeather/currentWeather.js
+++ frontend/src/components/currentWeather/currentWeather.js
@@ -12,18 +12,18 @@ const CurrentWeather = ({data}) => {
       </div>
 
       <div className='bottom'>
-  <p className='temperature'>{data.main.temp}°C</p>
+  <p className='temperature'>{Math.round(data.main.temp)}°C</p>
         <div className='details'>
             <div className='parameter-row'>
                 <span className='parameter-label top'>Details</span>
             </div>
             <div className='parameter-row'>
                 <span className='parameter-label'>Feels like: </span>
-                <span className='parameter-value'>{data.main.feels_like}°C</span>
+                <span className='parameter-value'>{Math.round(data.main.feels_like)}°C</span>
             </div>
             <div className='parameter-row'>
                 <span className='parameter-label'>Wind</span>
-  <span className='parameter-value'>{data.wind.speed} km/h</span>
+  <span className='parameter-value'>{data.wind.speed} m/s</span>
             </div>
             <div className='parameter-row'>
                 <span className='parameter-label'>Humidity</span>
@@ -31,7 +31,7 @@ const CurrentWeather = ({data}) => {
             </div>
             <div className='parameter-row'>
                 <span className='parameter-label'>Pressure</span>
-                <span className='parameter-value'>{data.main.pressure}</span>
+                <span className='parameter-value'>{data.main.pressure} hPa</span>
             </div>
         </div>
       </div>
