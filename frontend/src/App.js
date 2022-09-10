3f9154684ca2e76342dacafde448e3a11c723b90
nichro02 <nichro02@gmail.com>
2022-09-10 14:51:39 -0600

    Finished styling forecast details and formatted files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,50 @@
-import './App.css'
-import Search from './components/search/search'
-import CurrentWeather from './components/currentWeather/currentWeather'
-import Forecast from './components/forecast/forecast'
-import {WEATHER_API_URL} from './api'
-import {WeatherAPIKey} from './environment'
-import {useState} from 'react'
-
+import "./App.css";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/currentWeather/currentWeather";
+import Forecast from "./components/forecast/forecast";
+import { WEATHER_API_URL } from "./api";
+import { WeatherAPIKey } from "./environment";
+import { useState } from "react";
 
 function App() {
   //manage state for current weather and forecast
-  const [currentWeather, setCurrentWeather] = useState(null)
-  const [forecast, setForecast] = useState(null)
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
-    const [lat, lon] = searchData.value.split(' ')
-    console.log('LAT ---> ',lat,'LON ---> ', lon)
+    const [lat, lon] = searchData.value.split(" ");
+    console.log("LAT ---> ", lat, "LON ---> ", lon);
 
     //fetch current weather and forecast
     const currentWeatherFetch = fetch(
       `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WeatherAPIKey}&units=metric`
-    )
+    );
 
     const forecastFetch = fetch(
       `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WeatherAPIKey}&units=metric`
-    )
+    );
 
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
-        const weatherResponse = await response[0].json()
-        const forecastResponse = await response[1].json()
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
 
-        setCurrentWeather({city: searchData.label, ...weatherResponse})
-        setForecast({city: searchData.label, ...forecastResponse})
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
-  console.log('CURRENT WEATHER --> ', currentWeather)
-  console.log('FORECAST ---> ', forecast)
+  //console.log("CURRENT WEATHER --> ", currentWeather);
+  //console.log("FORECAST ---> ", forecast);
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather}/>}
-      {forecast && <Forecast data={forecast}/>}
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
 
-export default App
+export default App;
--- frontend/src/components/forecast/forecast.js
+++ frontend/src/components/forecast/forecast.js
@@ -6,59 +6,79 @@ import {
   AccordionItemButton,
 } from "react-accessible-accordion";
 
-import './forecast.css'
+import "./forecast.css";
 
-const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+const daysOfWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
 const Forecast = ({ data }) => {
   //determine day of week
-  const dayInWeek = new Date().getDay()
+  const dayInWeek = new Date().getDay();
   //order array so that dayInWeek is first element in array
-  const forecastDays = daysOfWeek.slice(dayInWeek, daysOfWeek.length).concat(daysOfWeek.slice(0, dayInWeek))
+  const forecastDays = daysOfWeek
+    .slice(dayInWeek, daysOfWeek.length)
+    .concat(daysOfWeek.slice(0, dayInWeek));
 
   //console.log(dayInWeek)
-  console.log(forecastDays)
+  //console.log(forecastDays);
 
-    return (
+  return (
     <>
       <label className="title">Upcoming Forecast</label>
       <Accordion allowZeroExpanded>
         {data.list.splice(0, 7).map((item, idx) => (
           <AccordionItem key={idx}>
             <AccordionItemHeading>
-                <AccordionItemButton>
-                    <div className='daily-item'>
-                        <img alt='weather' className='icon-small' src={`icons/${item.weather[0].icon}.png`}/>
-                        <label className='day'>{forecastDays[idx]}</label>
-                        <label className='description'>{item.weather[0].description}</label>
-                        <label className='min-max'>Low: {Math.round(item.main.temp_min)}°C</label>
-                        <label className='min-max'>High: {Math.round(item.main.temp_max)}°C</label>
-                    </div>
-                </AccordionItemButton>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    alt="weather"
+                    className="icon-small"
+                    src={`icons/${item.weather[0].icon}.png`}
+                  />
+                  <label className="day">{forecastDays[idx]}</label>
+                  <label className="description">
+                    {item.weather[0].description}
+                  </label>
+                  <label className="min-max">
+                    Low: {Math.round(item.main.temp_min)}°C
+                  </label>
+                  <label className="min-max">
+                    High: {Math.round(item.main.temp_max)}°C
+                  </label>
+                </div>
+              </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
-                <div className='daily-details-grid'>
-                <div className='daily-details-grid-item'>
-                        <label>Feels like </label>
-                        <label>{Math.round(item.main.feels_like) }°C</label>
-                    </div>
-                    <div className='daily-details-grid-item'>
-                        <label>Pressure </label>
-                        <label>{item.main.pressure} hPa</label>
-                    </div>
-                    <div className='daily-details-grid-item'>
-                        <label>Humidity </label>
-                        <label>{item.main.humidity}%</label>
-                    </div>
-                    <div className='daily-details-grid-item'>
-                        <label>Cloud cover </label>
-                        <label>{item.clouds.all}%</label>
-                    </div>
-                    <div className='daily-details-grid-item'>
-                        <label>Wind </label>
-                        <label>{item.wind.speed} m/s</label>
-                    </div>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label>Feels like </label>
+                  <label>{Math.round(item.main.feels_like)}°C</label>
                 </div>
+                <div className="daily-details-grid-item">
+                  <label>Pressure </label>
+                  <label>{item.main.pressure} hPa</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Humidity </label>
+                  <label>{item.main.humidity}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Cloud cover </label>
+                  <label>{item.clouds.all}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Wind </label>
+                  <label>{item.wind.speed} m/s</label>
+                </div>
+              </div>
             </AccordionItemPanel>
           </AccordionItem>
         ))}
