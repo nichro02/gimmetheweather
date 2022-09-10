bd0d3aa44a7d0b6157795044bb32e3323b2dfe49
nichro02 <nichro02@gmail.com>
2022-09-10 14:40:25 -0600

    Styled forecast details

--- frontend/src/components/forecast/forecast.js
+++ frontend/src/components/forecast/forecast.js
@@ -44,15 +44,15 @@ const Forecast = ({ data }) => {
                     </div>
                     <div className='daily-details-grid-item'>
                         <label>Pressure </label>
-                        <label>{item.main.pressure}</label>
+                        <label>{item.main.pressure} hPa</label>
                     </div>
                     <div className='daily-details-grid-item'>
                         <label>Humidity </label>
-                        <label>{item.main.humidity}</label>
+                        <label>{item.main.humidity}%</label>
                     </div>
                     <div className='daily-details-grid-item'>
-                        <label>Clouds </label>
-                        <label>{item.clouds.all}</label>
+                        <label>Cloud cover </label>
+                        <label>{item.clouds.all}%</label>
                     </div>
                     <div className='daily-details-grid-item'>
                         <label>Wind </label>
